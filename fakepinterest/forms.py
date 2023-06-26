05bd6e52a78879b716e07ec5f705c4df796d514d
tarcisiolopes <tarcisiolopesgomes@gmail.com>
2023-06-26 14:11:56 -0300

    Criar formulário de login e de criar conta

--- fakepinterest/forms.py
+++ fakepinterest/forms.py
@@ -1,2 +1,22 @@
 # formulários
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
+from fakepinterest.models import Usuario
 
+class FormLogin(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired()])
+    botao_confirmacao = SubmitField("Fazer login")
+
+class FormCriarConta(FlaskForm):
+    email = StringField("E-mail", validators=[DataRequired(), Email()])
+    senha = PasswordField("Senha", validators=[DataRequired(), Length(6,20)])
+    username = StringField("Nome", validators=[DataRequired()])
+    confirmacao_senha = PasswordField("Confirmação de senha", validators=[DataRequired(), EqualTo("Senha")])
+    botao_confirmacao = SubmitField("Criar Conta")
+
+    def validade_email(self, email):
+        usuario = Usuario.query.filter_by(email=email.data).first()
+        if usuario:
+            return ValidationError("E-mail já cadastrado, faça o login para continuar.")
