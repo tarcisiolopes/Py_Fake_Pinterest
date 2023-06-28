eb3c1bc3f853aef29a87ae505550f1cdb6ef02a8
tarcisiolopes <tarcisiolopesgomes@gmail.com>
2023-06-28 16:12:22 -0300

    Criando a funcionalidade de postar uma foto

--- fakepinterest/__init__.py
+++ fakepinterest/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from  flask_bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///comunidade.db"
--- fakepinterest/routes.py
+++ fakepinterest/routes.py
@@ -2,8 +2,10 @@
 from flask import render_template, url_for, redirect
 from fakepinterest import app, database, bcrypt
 from flask_login import login_required, login_user, logout_user, current_user
-from fakepinterest.forms import FormLogin, FormCriarConta
+from fakepinterest.forms import FormLogin, FormCriarConta, FormFoto
 from fakepinterest.models import Usuario, Foto
+import os
+from werkzeug.utils import secure_filename
 
 @app.route("/", methods=["GET", "POST"])
 def homepage():
@@ -35,14 +37,25 @@ def criar_conta():
         return redirect(url_for("perfil", id_usuario=usuario.id))
     return render_template("criar_conta.html", form=form_criar_conta)
 
-@app.route("/perfil/<id_usuario>")
+@app.route("/perfil/<id_usuario>", methods=["GET","POST"])
 @login_required
 def perfil(id_usuario):
     if int(id_usuario) == int(current_user.id):
-        return render_template("perfil.html", usuario=current_user)
+        form_foto = FormFoto()
+        if form_foto.validate_on_submit():
+            arquivo = form_foto.foto.data
+            nome_seguro = secure_filename(arquivo.filename)
+            caminho = os.path.join(os.path.abspath(os.path.dirname(__file__)),
+                              app.config["UPLOAD_FOLDER"],
+                              nome_seguro)
+            arquivo.save(caminho)
+            foto = Foto(imagem=nome_seguro, id_usuario=current_user.id)
+            database.session.add(foto)
+            database.session.commit()
+        return render_template("perfil.html", usuario=current_user, form=form_foto)
     else:
         usuario = Usuario.query.get(int(id_usuario))
-        return render_template("perfil.html", usuario=id_usuario)
+        return render_template("perfil.html", usuario=usuario, form=None)
 
 @app.route("/logout")
 @login_required
